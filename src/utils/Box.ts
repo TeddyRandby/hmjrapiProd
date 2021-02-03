cbdca45855e2434c24160537873f138e15066d19
Teddy Randby <tedrandby@gmail.com>
2021-02-03 09:28:17 -0500

    log box config

--- src/utils/Box.ts
+++ src/utils/Box.ts
@@ -1,6 +1,7 @@
 const BoxSDK = require("box-node-sdk");
 // Init Box connection
 let sdkConfig = process.env.BOX_CONFIG || require("../../boxconfig.json");
+console.log(sdkConfig)
 let sdk = BoxSDK.getPreconfiguredInstance(sdkConfig);
 const boxClient = sdk.getAppAuthClient("enterprise");
 export default boxClient;
