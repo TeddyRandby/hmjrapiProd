df3cc5bb7b956d40e9e13797c62c73656ad122f4
Teddy Randby <tedrandby@gmail.com>
2021-02-03 09:30:45 -0500

    compiled new changes

--- dist/utils/Box.js
+++ dist/utils/Box.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const BoxSDK = require("box-node-sdk");
 let sdkConfig = process.env.BOX_CONFIG || require("../../boxconfig.json");
+console.log(sdkConfig);
 let sdk = BoxSDK.getPreconfiguredInstance(sdkConfig);
 const boxClient = sdk.getAppAuthClient("enterprise");
 exports.default = boxClient;
