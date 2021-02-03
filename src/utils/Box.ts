f7b525c6d3489732066e1a7a21dc0b16b8320e38
Teddy Randby <tedrandby@gmail.com>
2021-02-03 09:38:44 -0500

    switched to ts-node

--- src/utils/Box.ts
+++ src/utils/Box.ts
@@ -1,7 +1,6 @@
 const BoxSDK = require("box-node-sdk");
 // Init Box connection
 let sdkConfig = process.env.BOX_CONFIG || require("../../boxconfig.json");
-console.log(sdkConfig)
 let sdk = BoxSDK.getPreconfiguredInstance(sdkConfig);
 const boxClient = sdk.getAppAuthClient("enterprise");
 export default boxClient;
